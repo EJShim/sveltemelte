78c048dc9c7aea12fc7ef0a97440287a2ad4c37a
EJShim <soulrommel@gmail.com>
2024-10-12 11:47:46 +0900

    asdf

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { createCollapsible, melt } from '@melt-ui/svelte'
+    const { open, root, content, trigger } = createCollapsible()
+  </script>
+   
+  <div {...$root} use:root>
+    <button {...$trigger} use:trigger>{$open ? 'Close' : 'Open'}</button>
+    <div {...$content} use:content>Obi-Wan says: Hello there!</div>
+  </div>
